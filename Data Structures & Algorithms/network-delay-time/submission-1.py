c324eef81b1f0a6e8365c83fb072476b59865785
Vivan-verma1109 <108888443+Vivan-verma1109@users.noreply.github.com>
2026-08-05 14:26:59 -0700

    Update: network-delay-time - submission-1

--- Data Structures & Algorithms/network-delay-time/submission-1.py
+++ Data Structures & Algorithms/network-delay-time/submission-1.py
@@ -1,33 +1,23 @@
-import heapq
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = {}
+        visited = set()
+        graph = defaultdict(list)
+        for a, b, c in times:
+            graph[a].append([b, c])
 
-        for i in range(n + 1):
-            graph[i] = []
-
-        for a, b, weight in times:
-            graph[a].append((b, weight))
-        dist = {}
-        for i in range(1, n + 1):
-            dist[i] = float("inf")
-        dist[k] = 0
         heap = [(0, k)]
+        t = 0
 
         while heap:
-            curDist, node = heapq.heappop(heap)
+            cost, node = heapq.heappop(heap)
 
-            if curDist > dist[node]:
+            if node in visited:
                 continue
 
-            for nxt, weight in graph[node]:
-                newDist = curDist + weight
-            
-                if newDist < dist[nxt]:
-                    dist[nxt] = newDist
-                    heapq.heappush(heap, (newDist, nxt))
-        ans = max(dist.values())
-        if ans == float("inf"):
-            return -1
+            t = max(t, cost)
+            visited.add(node)
 
-        return ans
+            for nxt, cost2 in graph[node]:
+                if nxt not in visited:
+                    heapq.heappush(heap, [(cost2 + cost), nxt])
+        return t if len(visited) == n else -1
